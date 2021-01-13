ad5b0cb1267873697e8b24b92c54eab5c483c3bc
HilanNorzhi <marcusfunke3@gmail.com>
2021-01-13 21:46:11 +0100

    correct naming of files

--- bench_test_1/plan_create.sh
+++ bench_test_1/plan_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clingo --out-atomf="%s." -c horizon=10 ./encoding_plan_create.lp ./robot1.lp | grep occurs > plan_r1.lp
+clingo --out-atomf="%s." -c horizon=10 ./encoding_plan_create.lp ./robot_1.lp | grep occurs > plan_r1.lp
 
-clingo --out-atomf="%s." -c horizon=10 ./encoding_plan_create.lp ./robot2.lp | grep occurs > plan_r2.lp
+clingo --out-atomf="%s." -c horizon=10 ./encoding_plan_create.lp ./robot_2.lp | grep occurs > plan_r2.lp
