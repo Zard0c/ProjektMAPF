6bec8133c5178c2b6befb63068dbf3aca3713193
HilanNorzhi <marcusfunke3@gmail.com>
2021-01-17 17:26:47 +0100

    copied bench_test_2 from Tom, Julian and Hannes

---
+++ bench_test_6/plan_create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clingo --out-atomf="%s." -c horizon=2 ./encoding_plan_create.lp ./robot_1.lp | grep occurs > plan_r1.lp
+
+clingo --out-atomf="%s." -c horizon=2 ./encoding_plan_create.lp ./robot_2.lp | grep occurs > plan_r2.lp
