91fce7d6eb1263dbd9b85d70158cffe365002fe0
HilanNorzhi <marcusfunke3@gmail.com>
2021-02-04 19:14:03 +0100

    upgraded the benchmark to the A domain

--- benchmarks/2-robots/domain_independend/bench_test_1/plan_create.sh
+++ benchmarks/2-robots/domain_independend/bench_test_1/plan_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clingo --out-atomf="%s." -c horizon=3 ./encoding_plan_create.lp ./robot_1.lp | grep occurs > plan_r1.lp
+clingo --out-atomf="%s." -c horizon=3 ./encoding_plan_create_M.lp ./robot_1.lp | grep occurs > plan_r1.lp
 
-clingo --out-atomf="%s." -c horizon=3 ./encoding_plan_create.lp ./robot_2.lp | grep occurs > plan_r2.lp
+clingo --out-atomf="%s." -c horizon=3 ./encoding_plan_create_M.lp ./robot_2.lp | grep occurs > plan_r2.lp
