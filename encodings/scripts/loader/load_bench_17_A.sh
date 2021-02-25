2f84f1374d6283e9cbeb7371de48e037d7d59b97
HilanNorzhi <marcusfunke3@gmail.com>
2021-02-25 19:48:09 +0100

    now also generates the merged plan

--- encodings/scripts/loader/load_bench_17_A.sh
+++ encodings/scripts/loader/load_bench_17_A.sh
@@ -8,4 +8,4 @@ cp ./benchmarks/2-robots/A-domain_only/bench_test_17/encoding_A.lp ./lab/instanc
 
 ./encodings/scripts/plan_creater/plan_create_17_A.sh
 
-#clingo --out-atomf="%s." -V0 -c horizon=7 ./lab/instances/encoding_17_A.lp ./lab/instances/instance_17.lp | grep object > ./lab/results/result_17_A.lp
+clingo --out-atomf="%s." -V0 -c horizon=15 ./lab/instances/encoding_17_A.lp ./lab/instances/instance_17.lp | grep occurs > ./lab/results/result_17_A.lp
