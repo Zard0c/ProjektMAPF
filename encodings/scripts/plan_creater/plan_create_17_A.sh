02d4724aeb7adf17c760529b8149fd00450192e3
HilanNorzhi <marcusfunke3@gmail.com>
2021-02-25 18:08:47 +0100

    added plan creater for bench 17

--- encodings/scripts/plan_creater/plan_create_17_A.sh
+++ encodings/scripts/plan_creater/plan_create_17_A.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clingo --out-atomf="%s." -c horizon=7 ./benchmarks/2-robots/A-domain_only/bench_test_17/encoding_plan_create_A.lp ./benchmarks/2-robots/A-domain_only/bench_test_17/robot_1.lp | grep occurs > ./lab/plans/plan_I17_r1_A.lp
+clingo --out-atomf="%s." -c horizon=9 ./benchmarks/2-robots/A-domain_only/bench_test_17/encoding_plan_create_A.lp ./benchmarks/2-robots/A-domain_only/bench_test_17/robot_1.lp | grep occurs > ./lab/plans/plan_I17_r1_A.lp
 
-clingo --out-atomf="%s." -c horizon=7 ./benchmarks/2-robots/A-domain_only/bench_test_17/encoding_plan_create_A.lp ./benchmarks/2-robots/A-domain_only/bench_test_17/robot_2.lp | grep occurs > ./lab/plans/plan_I17_r2_A.lp
+clingo --out-atomf="%s." -c horizon=9 ./benchmarks/2-robots/A-domain_only/bench_test_17/encoding_plan_create_A.lp ./benchmarks/2-robots/A-domain_only/bench_test_17/robot_2.lp | grep occurs > ./lab/plans/plan_I17_r2_A.lp
