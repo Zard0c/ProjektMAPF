b1b8ca74c891490f7577093462de7698cc073450
HilanNorzhi <marcusfunke3@gmail.com>
2021-03-13 15:04:57 +0100

    included path to scripts for all benchmarks

--- load_benchmark.sh
+++ load_benchmark.sh
@@ -2,19 +2,17 @@
 
 PS3='Choose the number listed on the left of your choice: '
 
-robots=("2 Robots" "3 Robots" "All" "Quit")
+robots=("2 Robots" "3 Robots" "4 Robots" "All" "Quit")
 all=("M-Domain" "A-Domain" "Both Domains" "All" "Back" "Quit")
 domain=("M-Domain" "A-Domain" "Both Domains" "Back" "Quit")
-ben2M=("Benchmark 1" "Benchmark 2" "Benchmark 3" "Benchmark 4" "Benchmark 5" "Benchmark 6" "Benchmark 7" "Benchmark 8" "Benchmark 9" "Back" "Quit")
+ben2M=("Benchmark 1" "Benchmark 2" "Benchmark 3" "Benchmark 4" "Benchmark 5" "Benchmark 6" "Benchmark 7" "Benchmark 8" "Benchmark 9" "Benchmark 16.3" "Benchmark 18.2" "Benchmark 18.2" "Back" "Quit")
 ben2O=("Benchmark 1" "Benchmark 4" "Benchmark 6" "Benchmark 7" "Back" "Quit")
 ben2A=("Benchmark 1" "Benchmark 4" "Benchmark 6" "Benchmark 7" "Benchmark 17" "Back" "Quit")
-ben3M=("Benchmark 10" "Benchmark 11" "Benchmark 12" "Back" "Quit")
-ben3O=("Back" "Quit")
-ben3A=("Back" "Quit")
+ben3M=("Benchmark 10" "Benchmark 11.1" "Benchmark 11.2" "Benchmark 12" "Benchmark 13" "Benchmark 14.1" "Benchmark 14.2" "Benchmark 14.3" "Back" "Quit")
+ben4M=("Benchmark 15.1" "Benchmark 15.2" "Benchmark 16.2" "Benchmark 16.2" "Back2" "Quit")
 
 echo "To load a benchmark, choose the amount of robots, the domain which should be compiled and then the benchmark"
 echo "You can go back one step by choosing Back, or leave midway by choosing Quit"
-echo "the A-domain is still WIP"
 
 select rob in "${robots[@]}"; do
 	case $rob in
@@ -39,6 +37,7 @@ select rob in "${robots[@]}"; do
 									;;
 								"Benchmark 5")
 									echo "loading benchmark 5 into lab"
+									./encodings/scripts/loader/load_bench_5_M.sh
 									;;
 								"Benchmark 8")
 									echo "loading benchmark 8 into lab"
@@ -47,6 +46,18 @@ select rob in "${robots[@]}"; do
 								"Benchmark 9")
 									echo "loading benchmark 9 into lab"
 									./encodings/scripts/loader/load_bench_9_M.sh
+									;;
+								"Benchmark 16.3")
+									echo "loading benchmark 16.3 into lab"
+									./encodings/scripts/loader/load_bench_16_3_M.sh
+									;;
+								"Benchmark 18.1")
+									echo "loading benchmark 18.1 into lab"
+									./encodings/scripts/loader/load_bench_18_1_M.sh
+									;;
+								"Benchmark 18.2")
+									echo "loading benchmark 18.2 into lab"
+									./encodings/scripts/loader/load_bench_18_2_M.sh
 									;;
 							#general benchmarks
 								"Benchmark 1")
@@ -172,78 +183,78 @@ select rob in "${robots[@]}"; do
 		"3 Robots")
 			echo "including all benchmarks for 3 robots"
 			echo
-			select dom in "${domain[@]}"; do
-				case $dom in
-					"M-Domain")
-						echo "including all benchmarks which work in the M domain"
-						echo
-						select ben in "${ben3M[@]}"; do
-							case $ben in
-							#M-Domain only
-								"Benchmark 10")
-									echo "loading benchmark 10 into lab"
-									./encodings/scripts/loader/load_bench_10_M.sh
-									;;
-								"Benchmark 11")
-									echo "loading benchmark 11 into lab"
-									./encodings/scripts/loader/load_bench_11_M.sh
-									;;
-								"Benchmark 12")
-									echo "loading benchmark 12 into lab"
-									./encodings/scripts/loader/load_bench_12_M.sh
-									;;
-							#general benchmarks
-								"Back")
-									break
-									;;
-								"Quit")
-									echo "closing loader"
-									exit
-									;;
-								*)
-									echo "invalid option $REPLY"
-									;;
-							esac
-						done
+			select ben in "${ben3M[@]}"; do
+				case $ben in
+				#M-Domain only
+					"Benchmark 10")
+						echo "loading benchmark 10 into lab"
+						./encodings/scripts/loader/load_bench_10_M.sh
 						;;
-					"A-Domain")
-						echo "including all benchmarks which work in the A domain"
-						echo
-						select ben in "${ben3A[@]}"; do
-							case $ben in
-							#A-Domain only
-							#general benchmarks
-								"Back")
-									break
-									;;
-								"Quit")
-									echo "closing loader"
-									exit
-									;;
-								*)
-									echo "invalid option $REPLY"
-									;;
-							esac
-						done
+					"Benchmark 11.1")
+						echo "loading benchmark 11.1 into lab"
+						./encodings/scripts/loader/load_bench_11_1_M.sh
 						;;
-					"Both Domains")
-						echo "including all benchmarks which work in A and M domain"
-						echo
-						select ben in "${ben3O[@]}"; do
-							case $ben in
-								"Back")
-									break
-									;;
-								"Quit")
-									echo "closing loader"
-									exit
-									;;
-								*)
-									echo "invalid option $REPLY"
-									;;
-							esac
-						done
+					"Benchmark 11.2")
+						echo "loading benchmark 11.2 into lab"
+						./encodings/scripts/loader/load_bench_11_2_M.sh
 						;;
+					"Benchmark 12")
+						echo "loading benchmark 12 into lab"
+						./encodings/scripts/loader/load_bench_12_M.sh
+						;;
+					"Benchmark 13")
+						echo "loading benchmark 13 into lab"
+						./encodings/scripts/loader/load_bench_13_M.sh
+						;;
+					"Benchmark 14.1")
+						echo "loading benchmark 14.1 into lab"
+						./encodings/scripts/loader/load_bench_14_1_M.sh
+						;;
+					"Benchmark 14.2")
+						echo "loading benchmark 14.2 into lab"
+						./encodings/scripts/loader/load_bench_14_2_M.sh
+						;;
+					"Benchmark 14.3")
+						echo "loading benchmark 14.3 into lab"
+						./encodings/scripts/loader/load_bench_14_3_M.sh
+						;;
+				#general benchmarks
+					"Back")
+						break
+						;;
+					"Quit")
+						echo "closing loader"
+						exit
+						;;
+					*)
+						echo "invalid option $REPLY"
+						;;
+				esac
+			done
+			;;
+		"4 Robots")
+			echo "including all benchmarks for 4 robots"
+			echo
+			select ben in "${ben4M[@]}"; do
+				case $ben in
+				#M-Domain only
+					"Benchmark 15.1")
+						echo "loading benchmark 15.1 into lab"
+						./encodings/scripts/loader/load_bench_15_1_M.sh
+						;;
+					"Benchmark 15.2")
+						echo "loading benchmark 15.2 into lab"
+						./encodings/scripts/loader/load_bench_15_2_M.sh
+						;;
+					"Benchmark 16.1")
+						echo "loading benchmark 16.1 into lab"
+						./encodings/scripts/loader/load_bench_16_1_M.sh
+						;;
+					"Benchmark 16.2")
+						echo "loading benchmark 16.2 into lab"
+						./encodings/scripts/loader/load_bench_16_2_M.sh
+						;;
+				#general benchmarks
 					"Back")
 						break
 						;;
@@ -273,8 +284,20 @@ select rob in "${robots[@]}"; do
 						./encodings/scripts/loader/load_bench_8_M.sh
 						./encodings/scripts/loader/load_bench_9_M.sh
 						./encodings/scripts/loader/load_bench_10_M.sh
-						./encodings/scripts/loader/load_bench_11_M.sh
+						./encodings/scripts/loader/load_bench_11_1_M.sh
+						./encodings/scripts/loader/load_bench_11_2_M.sh
 						./encodings/scripts/loader/load_bench_12_M.sh
+						./encodings/scripts/loader/load_bench_13_M.sh
+						./encodings/scripts/loader/load_bench_14_1_M.sh
+						./encodings/scripts/loader/load_bench_14_2_M.sh
+						./encodings/scripts/loader/load_bench_14_3_M.sh
+						./encodings/scripts/loader/load_bench_15_1_M.sh
+						./encodings/scripts/loader/load_bench_15_2_M.sh
+						./encodings/scripts/loader/load_bench_16_1_M.sh
+						./encodings/scripts/loader/load_bench_16_2_M.sh
+						./encodings/scripts/loader/load_bench_16_3_M.sh
+						./encodings/scripts/loader/load_bench_18_1_M.sh
+						./encodings/scripts/loader/load_bench_18_2_M.sh
 						exit
 						;;
 					"A-Domain")
@@ -307,8 +330,20 @@ select rob in "${robots[@]}"; do
 						./encodings/scripts/loader/load_bench_8_M.sh
 						./encodings/scripts/loader/load_bench_9_M.sh
 						./encodings/scripts/loader/load_bench_10_M.sh
-						./encodings/scripts/loader/load_bench_11_M.sh
+						./encodings/scripts/loader/load_bench_11_1_M.sh
+						./encodings/scripts/loader/load_bench_11_2_M.sh
 						./encodings/scripts/loader/load_bench_12_M.sh
+						./encodings/scripts/loader/load_bench_13_M.sh
+						./encodings/scripts/loader/load_bench_14_1_M.sh
+						./encodings/scripts/loader/load_bench_14_2_M.sh
+						./encodings/scripts/loader/load_bench_14_3_M.sh
+						./encodings/scripts/loader/load_bench_15_1_M.sh
+						./encodings/scripts/loader/load_bench_15_2_M.sh
+						./encodings/scripts/loader/load_bench_16_1_M.sh
+						./encodings/scripts/loader/load_bench_16_2_M.sh
+						./encodings/scripts/loader/load_bench_16_3_M.sh
+						./encodings/scripts/loader/load_bench_18_1_M.sh
+						./encodings/scripts/loader/load_bench_18_2_M.sh
 					#A-Domain
 						./encodings/scripts/loader/load_bench_1_A.sh
 						./encodings/scripts/loader/load_bench_4_A.sh
