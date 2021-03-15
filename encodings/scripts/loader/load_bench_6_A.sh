690b43e94878205e90cd15d8ac6af29370120371
HilanNorzhi <marcusfunke3@gmail.com>
2021-03-15 15:31:31 +0100

    small bugfixes

--- encodings/scripts/loader/load_bench_6_A.sh
+++ encodings/scripts/loader/load_bench_6_A.sh
@@ -8,4 +8,4 @@ cp ./benchmarks/2-robots/domain_independend/bench_test_6/encoding_A.lp ./lab/ins
 
 ./encodings/scripts/plan_creater/plan_create_6_A.sh
 
-clingo --out-atomf="%s." -V0 -c horizon=12 ./lab/instances/encoding_6_A.lp ./lab/instances/instance_6.lp | grep object > ./lab/results/result_6_A.lp
+clingo --out-atomf="%s." -V0 -c horizon=14 ./lab/instances/encoding_6_A.lp ./lab/instances/instance_6.lp | grep object > ./lab/results/result_6_A.lp
